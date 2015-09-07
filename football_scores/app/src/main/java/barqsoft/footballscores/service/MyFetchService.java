10ce66f55aa9268e313bc9d5fb8dab7bad53bb8f
jrarama <joshuararama@gmail.com>
2015-09-08 01:15:37 +0800

    Insert test data when no matches available

--- football_scores/app/src/main/java/barqsoft/footballscores/service/MyFetchService.java
+++ football_scores/app/src/main/java/barqsoft/footballscores/service/MyFetchService.java
@@ -1,39 +1,32 @@
 package barqsoft.footballscores.service;
 
 import android.app.IntentService;
-import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.annotation.Nullable;
-import android.text.TextUtils;
 import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.TimeZone;
 
 import barqsoft.footballscores.DatabaseContract;
+import barqsoft.footballscores.DatabaseContract.SeasonsTable;
 import barqsoft.footballscores.R;
 import barqsoft.footballscores.Utilies;
+
+import static barqsoft.footballscores.DatabaseContract.*;
 
 /**
  * Created by yehya khaled on 3/2/2015.
@@ -109,11 +102,13 @@ public class MyFetchService extends IntentService {
                 // This bit is to check if the data contains any matches. If not, we call processJson on the dummy data
                 JSONArray matches = new JSONObject(json).getJSONArray("fixtures");
                 if (matches.length() == 0) {
-                    Log.d(LOG_TAG, "No fixtures");
+                    //if there is no data, call the function on dummy data
+                    //this is expected behavior during the off season.
+                    processScoresData(getString(R.string.dummy_data), getApplicationContext(), false);
                     return;
                 }
 
-                processScoresdata(json, context);
+                processScoresData(json, getApplicationContext(), true);
             } else {
                 //Could not Connect
                 Log.e(LOG_TAG, "No data received.");
@@ -159,9 +154,9 @@ public class MyFetchService extends IntentService {
                 String crestUrl = team.getString(CREST_URL);
 
                 ContentValues teamValues = new ContentValues();
-                teamValues.put(DatabaseContract.TeamsTable._ID, id);
-                teamValues.put(DatabaseContract.TeamsTable.NAME_COLUMN, name);
-                teamValues.put(DatabaseContract.TeamsTable.CREST_URL_COLUMN, crestUrl);
+                teamValues.put(TeamsTable._ID, id);
+                teamValues.put(TeamsTable.NAME_COLUMN, name);
+                teamValues.put(TeamsTable.CREST_URL_COLUMN, crestUrl);
                 Log.d(LOG_TAG, "Team Id: " + id + ", Name: " + name
                         + ", CrestUrl: " + crestUrl);
                 values.add(teamValues);
@@ -170,7 +165,7 @@ public class MyFetchService extends IntentService {
             ContentValues[] contentValues = new ContentValues[values.size()];
             values.toArray(contentValues);
             int insertedData = context.getContentResolver().bulkInsert(
-                    DatabaseContract.TeamsTable.buildTeamsPath(), contentValues);
+                    TeamsTable.buildTeamsPath(), contentValues);
 
             Log.d(LOG_TAG, "Succesfully Inserted " + insertedData + " data.");
         } catch (JSONException e) {
@@ -202,9 +197,9 @@ public class MyFetchService extends IntentService {
 
                 leagueMap.put(seasonId, league);
                 ContentValues seasonValues = new ContentValues();
-                seasonValues.put(DatabaseContract.SeasonsTable.SEASON_ID_COLUMN, seasonId);
-                seasonValues.put(DatabaseContract.SeasonsTable.CAPTION_COLUMN, caption);
-                seasonValues.put(DatabaseContract.SeasonsTable.LEAGUE_COLUMN, league);
+                seasonValues.put(SeasonsTable.SEASON_ID_COLUMN, seasonId);
+                seasonValues.put(SeasonsTable.CAPTION_COLUMN, caption);
+                seasonValues.put(SeasonsTable.LEAGUE_COLUMN, league);
 
                 Log.d(LOG_TAG, "Seasons Id: " + seasonId + ", League: " + league
                         + ", Caption: " + caption);
@@ -216,7 +211,7 @@ public class MyFetchService extends IntentService {
             ContentValues[] contentValues = new ContentValues[values.size()];
             values.toArray(contentValues);
             int insertedData = context.getContentResolver().bulkInsert(
-                    DatabaseContract.SeasonsTable.buildSeasonsPath(), contentValues);
+                    SeasonsTable.buildSeasonsPath(), contentValues);
 
             Log.d(LOG_TAG, "Succesfully Inserted " + insertedData + " data.");
 
@@ -226,8 +221,11 @@ public class MyFetchService extends IntentService {
     }
 
 
-    private void processScoresdata(String json, Context context) {
+    private void processScoresData(String json, Context context, boolean isReal) {
         //JSON data
+        if (!isReal) {
+            insertFakeData(context);
+        }
 
         final String SOCCER_SEASON = "soccerseason";
         final String MATCH_DATE = "date";
@@ -251,12 +249,17 @@ public class MyFetchService extends IntentService {
                 JSONObject links = match.getJSONObject(LINKS);
                 String league = getIdFromUrl(links.getJSONObject(SOCCER_SEASON).getString(HREF));
 
-                if (!leagueMap.containsKey(league)) {
+                if (isReal && !leagueMap.containsKey(league)) {
                     Log.d(LOG_TAG, "League " + league + " not in wanted list");
                     continue;
                 }
 
                 String matchId = getIdFromUrl(links.getJSONObject(SELF).getString(HREF));
+                if(!isReal){
+                    //This if statement changes the match ID of the dummy data so that it all goes into the database
+                    matchId=matchId+Integer.toString(i);
+                }
+
                 Integer homeTeam = Integer.parseInt(getIdFromUrl(links.getJSONObject(HOME_TEAM_LINK).getString(HREF)));
                 Integer awayTeam = Integer.parseInt(getIdFromUrl(links.getJSONObject(AWAY_TEAM_LINK).getString(HREF)));
 
@@ -272,6 +275,13 @@ public class MyFetchService extends IntentService {
                     matchDate = newDateFormat.format(parsedDate);
                     matchTime = matchDate.substring(matchDate.indexOf(":") + 1);
                     matchDate = matchDate.substring(0, matchDate.indexOf(":"));
+
+                    if(!isReal){
+                        //This if statement changes the dummy data's date to match our current date range.
+                        Date fragmentdate = new Date(System.currentTimeMillis()+((i-2)*86400000));
+                        SimpleDateFormat mformat = new SimpleDateFormat("yyyy-MM-dd");
+                        matchDate=mformat.format(fragmentdate);
+                    }
                 } catch (Exception e) {
                     Log.e(LOG_TAG, "Error parsing date", e);
                 }
@@ -281,17 +291,17 @@ public class MyFetchService extends IntentService {
                 String awayGoals = match.getJSONObject(RESULT).getString(AWAY_GOALS);
                 String matchDay = match.getString(MATCH_DAY);
                 ContentValues matchValues = new ContentValues();
-                matchValues.put(DatabaseContract.ScoresTable.MATCH_ID, matchId);
-                matchValues.put(DatabaseContract.ScoresTable.DATE_COL, matchDate);
-                matchValues.put(DatabaseContract.ScoresTable.TIME_COL, matchTime);
-                matchValues.put(DatabaseContract.ScoresTable.HOME_COL, home);
-                matchValues.put(DatabaseContract.ScoresTable.AWAY_COL, away);
-                matchValues.put(DatabaseContract.ScoresTable.HOME_TEAM_COL, homeTeam);
-                matchValues.put(DatabaseContract.ScoresTable.AWAY_TEAM_COL, awayTeam);
-                matchValues.put(DatabaseContract.ScoresTable.HOME_GOALS_COL, homeGoals);
-                matchValues.put(DatabaseContract.ScoresTable.AWAY_GOALS_COL, awayGoals);
-                matchValues.put(DatabaseContract.ScoresTable.LEAGUE_COL, league);
-                matchValues.put(DatabaseContract.ScoresTable.MATCH_DAY, matchDay);
+                matchValues.put(ScoresTable.MATCH_ID, matchId);
+                matchValues.put(ScoresTable.DATE_COL, matchDate);
+                matchValues.put(ScoresTable.TIME_COL, matchTime);
+                matchValues.put(ScoresTable.HOME_COL, home);
+                matchValues.put(ScoresTable.AWAY_COL, away);
+                matchValues.put(ScoresTable.HOME_TEAM_COL, homeTeam);
+                matchValues.put(ScoresTable.AWAY_TEAM_COL, awayTeam);
+                matchValues.put(ScoresTable.HOME_GOALS_COL, homeGoals);
+                matchValues.put(ScoresTable.AWAY_GOALS_COL, awayGoals);
+                matchValues.put(ScoresTable.LEAGUE_COL, league);
+                matchValues.put(ScoresTable.MATCH_DAY, matchDay);
 
                 Log.d(LOG_TAG, "MatchID: " + matchId + ", League: " + league
                     + ", Match Day: " + matchDay + ", Date: "+ matchDate + ", Time: " + matchTime
@@ -305,12 +315,40 @@ public class MyFetchService extends IntentService {
             values.toArray(contentValues);
 
             int insertedData = context.getContentResolver().bulkInsert(
-                    DatabaseContract.BASE_CONTENT_URI, contentValues);
+                    BASE_CONTENT_URI, contentValues);
 
             Log.d(LOG_TAG, "Succesfully Inserted " + insertedData + " data.");
         } catch (JSONException e) {
             Log.e(LOG_TAG, e.getMessage());
         }
+    }
+
+    private void insertFakeData(Context context) {
+        ContentValues seasonValues = new ContentValues();
+        seasonValues.put(SeasonsTable.SEASON_ID_COLUMN, 357);
+        seasonValues.put(SeasonsTable.CAPTION_COLUMN, "TESTING SEASON");
+        seasonValues.put(SeasonsTable.LEAGUE_COLUMN, "TS");
+
+        int insertedData = context.getContentResolver().bulkInsert(
+                SeasonsTable.buildSeasonsPath(), new ContentValues[] {seasonValues});
+
+        Log.d(LOG_TAG, "Succesfully Inserted " + insertedData + " fake seasons.");
+
+        ContentValues homeTeam = new ContentValues();
+        homeTeam.put(TeamsTable._ID, 263);
+        homeTeam.put(TeamsTable.NAME_COLUMN, "Test Home Team");
+        homeTeam.put(TeamsTable.CREST_URL_COLUMN, "https://upload.wikimedia.org/wikipedia/en/2/2e/Deportivo_Alaves_logo.svg");
+
+        ContentValues awayTeam = new ContentValues();
+        awayTeam.put(TeamsTable._ID, 275);
+        awayTeam.put(TeamsTable.NAME_COLUMN, "Test Home Team");
+        awayTeam.put(TeamsTable.CREST_URL_COLUMN, "https://upload.wikimedia.org/wikipedia/en/2/20/UD_Las_Palmas_logo.svg");
+
+        insertedData = context.getContentResolver().bulkInsert(
+                TeamsTable.buildTeamsPath(), new ContentValues[] {homeTeam, awayTeam});
+
+        Log.d(LOG_TAG, "Succesfully Inserted " + insertedData + " fake teams.");
+
     }
 }
 
